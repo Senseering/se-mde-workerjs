4df2c87e4435a9b1e72519c2487ac6fad4d5927f
kristof <kristof.herrmann@rwth-aachen.de>
2020-04-28 19:40:14 +0200

    adding logs every 30 seconds

--- examples/loadtest/index.js
+++ examples/loadtest/index.js
@@ -9,9 +9,18 @@ let exampleJson = require("./data/4kb.json");
 (async function () {
     await worker.connect()
     let counter = 0
+    let start = Date.now()
+    let intervall = 30//seconds
+
     while (true) {
         await worker.publish({ test: exampleJson })
-        counter++       
+        counter++
+
+        if (Date.now() - start > 10 * 1000) {
+            console.log("datapieces (absolut):\t" + counter + "\t per second:\t" + counter / intervall + " \t \t KB/s: \t" + (counter * 4) / intervall)
+            counter = 0
+            start = Date.now()
+        }
     }
     await worker.disconnect()
 })();
