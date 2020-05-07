57e8d8bb9ad4d0414e3e160a1c35277f50d77824
Ubuntu <ubuntu@ip-172-31-6-49.eu-central-1.compute.internal>
2020-05-07 09:53:55 +0000

    mod schema

--- examples/spssniffer/index.js
+++ examples/spssniffer/index.js
@@ -15,13 +15,11 @@ server.use(bodyParser.json({ limit: '16mb' }))
 server.use(bodyParser.urlencoded({ limit: '16mb', extended: true }))
 server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true }));
-
+  
 (async function () {
     await worker.connect(config)
     server.use('/', async (req, res) => {
-        console.log("received")
-        await worker.publish(req.body)
-        console.log("published")
+        await worker.publish(req.body,{ttl:60*60*1000})
         res.status(200).send()
     });
     server.listen(3001, () => console.log(`Node listening on port 3001`))
